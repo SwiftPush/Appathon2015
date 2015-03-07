2cd1509bf9936a6afa0d89297f1d5dcf09332da9
Sam <sam.healr@gmail.com>
2015-03-07 17:46:19 +0000

    bugfix on closing

--- app/src/main/java/com/lemonslice/appathon/CameraPreview.java
+++ app/src/main/java/com/lemonslice/appathon/CameraPreview.java
@@ -61,7 +61,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         assert(camera != null);
-
+        camera.setPreviewCallback(null);
         camera.stopPreview();
         camera.release();
         camera = null;
