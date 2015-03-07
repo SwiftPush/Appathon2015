bea3f146e39d3c28fd35ac65f931ceec670bdd7e
20k <the_berronator@hotmail.co.uk>
2015-03-07 11:55:41 +0000

    whatever was in my branch

--- app/src/main/java/com/lemonslice/appathon/MainActivity.java
+++ app/src/main/java/com/lemonslice/appathon/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.opencv.core.*;
 
 
 public class MainActivity extends Activity {
