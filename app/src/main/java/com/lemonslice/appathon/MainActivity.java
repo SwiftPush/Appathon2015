c9533e72d9087c107faaa64dc2e8d63d9728c41f
Alexander Hill <me@alxhill.com>
2015-03-08 06:47:49 +0000

    It does an internet

--- app/src/main/java/com/lemonslice/appathon/MainActivity.java
+++ app/src/main/java/com/lemonslice/appathon/MainActivity.java
@@ -1,13 +1,18 @@
 package com.lemonslice.appathon;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.hardware.Camera;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
 import android.widget.FrameLayout;
 import android.widget.ScrollView;
+
+import java.net.InetAddress;
 
 public class MainActivity extends Activity {
 
@@ -18,9 +23,101 @@ public class MainActivity extends Activity {
 
     static int hello = 0;
 
+    private String SERVICE_NAME = "Yomoji";
+    private String SERVICE_TYPE = "_http._tcp";
+    private NsdManager mNsdManager;
+
+    NsdManager.RegistrationListener registrationListener = new NsdManager.RegistrationListener() {
+        @Override
+        public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            Log.d("NSD", "fail reg");
+        }
+
+        @Override
+        public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            Log.d("NSD", "fail unreg");
+        }
+
+        @Override
+        public void onServiceRegistered(NsdServiceInfo serviceInfo) {
+            SERVICE_NAME = serviceInfo.getServiceName();
+            Log.d("NSD", "Registered name: " + SERVICE_NAME);
+        }
+
+        @Override
+        public void onServiceUnregistered(NsdServiceInfo serviceInfo) {
+            Log.d("NSD", "Unregistered name: " + SERVICE_NAME);
+        }
+    };
+
+    NsdManager.DiscoveryListener discoveryListener = new NsdManager.DiscoveryListener() {
+        @Override
+        public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+            Log.d("NSD", String.format("disc start fail for %s err %d", serviceType, errorCode));
+        }
+
+        @Override
+        public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+            Log.d("NSD", "stop fail");
+        }
+
+        @Override
+        public void onDiscoveryStarted(String serviceType) {
+            Log.d("NSD", "disc start " + serviceType);
+        }
+
+        @Override
+        public void onDiscoveryStopped(String serviceType) {
+            Log.d("NSD", "disc stop " + serviceType);
+        }
+
+        @Override
+        public void onServiceFound(NsdServiceInfo serviceInfo) {
+            String TAG = "NSD";
+            Log.d(TAG, "SERVICE FOUND BITCH: " + serviceInfo.toString());
+            Log.d(TAG, "Service discovery success : " + serviceInfo);
+            Log.d(TAG, "Host = " + serviceInfo.getServiceName());
+
+            if (serviceInfo.getServiceName().equals(SERVICE_NAME)) {
+                Log.d(TAG, "lol found myself ;)");
+            } else {
+                Log.d(TAG, "FOUND SOME OTHER FUCKER!!!!! :D");
+                mNsdManager.resolveService(serviceInfo, resolveListener);
+            }
+
+        }
+
+        @Override
+        public void onServiceLost(NsdServiceInfo serviceInfo) {
+            Log.d("NSD", "Service lost");
+        }
+    };
+
+    NsdManager.ResolveListener resolveListener = new NsdManager.ResolveListener() {
+        @Override
+        public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            Log.d("NSD", "Resolve failed");
+        }
+
+        @Override
+        public void onServiceResolved(NsdServiceInfo serviceInfo) {
+            Log.d("NSD", "Resolve worked???!!");
+
+            InetAddress hostAddress = serviceInfo.getHost();
+            int hostPort = serviceInfo.getPort();
+
+            Log.d("NSD", "addr: " + hostAddress.toString() + " port: " + hostPort);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+        registerService(9000);
+        mNsdManager.discoverServices(SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, discoveryListener);
+
         setContentView(R.layout.activity_main);
 
         camera = CameraPreview.openFrontCamera();
@@ -64,6 +161,17 @@ public class MainActivity extends Activity {
                 }
             }
         });
+    }
+
+    private void registerService(int port) {
+        NsdServiceInfo serviceInfo = new NsdServiceInfo();
+        serviceInfo.setServiceName(SERVICE_NAME);
+        serviceInfo.setServiceType(SERVICE_TYPE);
+        serviceInfo.setPort(port);
+
+        mNsdManager.registerService(serviceInfo,
+                NsdManager.PROTOCOL_DNS_SD,
+                registrationListener);
     }
 
     @Override
